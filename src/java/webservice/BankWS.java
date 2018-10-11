96ff42cbc85566cf062edf5be13d9d0f5da331d0
vlcpl <valentin.lecouple@gmail.com>
2018-10-11 11:35:44 +0200

    [WS] Ajout des méthodes demandées par l'énoncé

--- src/java/webservice/BankWS.java
+++ src/java/webservice/BankWS.java
@@ -5,6 +5,11 @@
  */
 package webservice;
 
+import DAO.DAO;
+import beans.Account;
+import beans.BankBranch;
+import beans.Client;
+import java.util.List;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -23,5 +28,30 @@ public class BankWS {
     @WebMethod(operationName = "hello")
     public String hello(@WebParam(name = "name") String txt) {
         return "Hello " + txt + " !";
+    }
+    
+    @WebMethod(operationName = "findClientById")
+    public Client findClientById(@WebParam(name = "clientId") String clientId){
+        DAO<Client> DAOClient = new DAO();
+        return (Client)DAOClient.findByPrimaryKey(new Client(), clientId);
+    }
+    
+    @WebMethod(operationName = "getAllClients")
+    public List<Client> getAllClients(){
+        DAO<Client> DAOClient = new DAO();
+        return (List<Client>)DAOClient.findAll(new Client());
+    }
+    
+    @WebMethod(operationName = "createBankBranch")
+    public void createBankBranch(@WebParam(name = "numAgence") String numAgence, @WebParam(name = "adresseAgence") String adresseAgence) throws Exception{
+        BankBranch newBankBranch = new BankBranch(numAgence, adresseAgence);
+        DAO<BankBranch> DAOBankBranch = new DAO();
+        DAOBankBranch.create(newBankBranch);
+    }
+    
+    @WebMethod(operationName = "deleteAccount")
+    public void deleteAccount(@WebParam(name = "numAccount") String numAccount){
+        DAO<Account> DAOAccount = new DAO();
+        DAOAccount.delete((Account)DAOAccount.findByPrimaryKey(new Account(), numAccount));
     }
 }
